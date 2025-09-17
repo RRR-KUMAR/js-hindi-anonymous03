b7f82e7e055641f4c51c0ed6e71f4bdf621dadd2
RRR-KUMAR <03anonymous143@gmail.com>
2025-09-17 15:17:24 +0000

    container config done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,25 @@
+const account_Id = 1444553
+let accountEmail = "anonymous03@gmail.com"
+var accountPassword = '12334'
+accountCity = "Pandatarai"
+let accountstate;
+
+
+// accountId = 2 // not allowed
+
+accountEmail = 'hc@hc.com'
+accountPassword = 1313131414
+accountCity = "Barela"
+
+console.log(account_Id);
+
+
+/*
+Prefer note to use var.
+Becouse of the issue in block scope and functional scape.
+
+*/
+
+
+console.table([accountEmail, accountPassword, accountCity, accountstate]) /* all variable write in one way Better then up */
+
--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,5 @@
 console.log("Ram AND DJ")
+
+
+
+
