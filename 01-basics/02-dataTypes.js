1e4c8ac384391f09eb47a9ed275b0fd16a273dea
RRR-KUMAR <03anonymous143@gmail.com>
2025-09-18 16:50:30 +0000

    dataTyeps

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,33 @@
+"use stict"; // treat all js code newer version
+
+// alert(3 + 3) // we aer using nodejs, not browser 
+
+
+
+console.log(3
+    + 3)  // code readebilty should be high
+    
+
+console.log("Rahul and Ram")
+
+
+let name = "Ram"
+let age = 22
+let isLoggedId = false
+let state;
+
+
+
+// number => to power 53
+// bigint
+// stirng => ""
+// boolean => true/false
+// null =>  standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefine
+console.log(typeof null); // object
