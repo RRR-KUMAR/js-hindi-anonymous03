66dba4e5dcba68eb8fb79a27da171e6ccbf295ca
RRR-KUMAR <03anonymous143@gmail.com>
2025-09-20 05:54:00 +0000

    conversionOFdatatypes

---
+++ 01-basics/03-conversionOperaston.js
@@ -0,0 +1,31 @@
+let score = "Rahul Kumar"  
+
+ 
+//console.log(typeof  (score));
+//console.log(typeof  (score));
+
+let valueInnumber = Number((score))
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+// "33" => 33
+// "33abc" =>
+// true => 1; false => 0
+
+
+let isLoggedIn = "Rider"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+//"" => false
+// "Rider" => true
+
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
