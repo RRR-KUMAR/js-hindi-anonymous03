0651d2926c037f803e8da5d5f2245589e15b4cca
RRR-KUMAR <03anonymous143@gmail.com>
2025-10-06 11:27:41 +0000

    datatypes summry

---
+++ 05-datatypes-summary.js
@@ -0,0 +1,49 @@
+// primitive
+
+/* 7 types : String, Number, Boolean, null, undefined, Symbol, */
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id == anotherId);
+
+//const bigNumber = 123361814387678603n
+
+
+
+
+// Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "kli"] 
+let myObj = {
+    name: "Ram",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello Word");
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof Function);
+console.log(typeof heros);
+console.log(typeof anotherId;
+
+
+// Link to study
+// https://tc39.es/ecma262/multipage/ecmascript-data-types-and-values.html
+    
