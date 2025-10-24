22ee5a8d8ee1bc0d6aec74eddbffc5a7803dfa02
RRR-KUMAR <03anonymous143@gmail.com>
2025-10-24 06:22:31 +0000

    Move

---
+++ 02-basics/03-objects.js
@@ -0,0 +1,19 @@
+// singleton 
+
+//  object literals
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+name: RRR,
+location: "Bharat",
+email: "ram03@gmail.com",
+isLoggedIn: false,
+lastLoginDays: ["Monday"],
+
+}
+
+console.log(jsuser.email)
+console.log(jsuser.["email"])
+console.log(typeof jsuser.[mySym])
+
