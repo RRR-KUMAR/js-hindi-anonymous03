ce5a2a08b6c99448c81388cbcfdb59389f91d84d
RRR-KUMAR <03anonymous143@gmail.com>
2025-11-12 12:30:59 +0000

    scopes

--- 03-basics/02-scopes.js
+++ 03-basics/02-scopes.js
@@ -5,7 +5,7 @@ let a = 300
 if (true) {
    let a = 10
    const b = 20
-   console.log("INNER: ", a);
+   // console.log("INNER: ", a);
 
 }
 
@@ -14,3 +14,47 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+function one(){
+   const username = "Ram"
+
+   function tow(){
+      const website = "youtube"
+      // console.log(username);
+   }
+   // console.log(website);
+
+   tow()
+}
+
+one()
+
+if (true) {
+   const username = "Ram"
+   if (username === "Ram") {
+      const  website = " youtube"
+      // console.log(username + website);
+   }
+   //  console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++  intresting ++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+   return num + 1
+
+}
+addTow(5)
+
+const addTow = function(num){
+   return num + 2
+}
+
